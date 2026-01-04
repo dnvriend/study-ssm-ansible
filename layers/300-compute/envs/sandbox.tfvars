fb7781be6177db4f11676dbc4729252b326d7e55
Dennis Vriend <dnvriend@gmail.com>
2026-01-04 15:33:16 +0100

    task: Task 3.1: Compute AMI

---
+++ layers/300-compute/envs/sandbox.tfvars
@@ -0,0 +1,9 @@
+# Sandbox environment variables for 300-compute layer
+
+environment        = "sandbox"
+aws_region        = "eu-central-1"
+aws_account_id    = "862378407079"
+
+web_instance_type    = "t3.micro"
+app_instance_type    = "t3.small"
+bastion_instance_type = "t3.micro"
--- layers/300-compute/variables.tf
+++ layers/300-compute/variables.tf
@@ -9,3 +9,38 @@ variable "aws_account_id" {
   type        = string
   default     = "123456789012"
 }
+
+variable "environment" {
+  description = "Environment name (e.g., sandbox, dev, prod)"
+  type        = string
+}
+
+variable "web_instance_type" {
+  description = "EC2 instance type for web servers"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "app_instance_type" {
+  description = "EC2 instance type for application servers"
+  type        = string
+  default     = "t3.small"
+}
+
+variable "bastion_instance_type" {
+  description = "EC2 instance type for bastion host"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "web_server_count" {
+  description = "Number of web server instances to create"
+  type        = number
+  default     = 2
+}
+
+variable "app_server_count" {
+  description = "Number of application server instances to create"
+  type        = number
+  default     = 2
+}
