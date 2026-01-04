a0014ca228151f12443ea723e51af65cbb8085aa
Dennis Vriend <dnvriend@gmail.com>
2026-01-04 15:04:34 +0100

    task: Task 2.1: Network Layer VPC

--- layers/100-network/outputs.tf
+++ layers/100-network/outputs.tf
@@ -1,1 +1,36 @@
-# Add your outputs here
+# 100-network Layer Outputs
+
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "vpc_cidr" {
+  description = "CIDR block of the VPC"
+  value       = aws_vpc.main.cidr_block
+}
+
+output "internet_gateway_id" {
+  description = "ID of the Internet Gateway"
+  value       = aws_internet_gateway.main.id
+}
+
+output "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  value       = aws_subnet.public[*].id
+}
+
+output "public_subnet_cidrs" {
+  description = "List of public subnet CIDR blocks"
+  value       = aws_subnet.public[*].cidr_block
+}
+
+output "availability_zones" {
+  description = "Availability zones in use"
+  value       = var.availability_zones
+}
+
+output "public_route_table_id" {
+  description = "ID of the public route table"
+  value       = aws_route_table.public.id
+}
