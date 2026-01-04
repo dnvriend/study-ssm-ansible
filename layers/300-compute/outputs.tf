eafa1d5adce04c7fd387e88286287026a2e8a8c5
Dennis Vriend <dnvriend@gmail.com>
2026-01-04 15:34:38 +0100

    task: Task 3.2: Web Servers

--- layers/300-compute/outputs.tf
+++ layers/300-compute/outputs.tf
@@ -1,1 +1,17 @@
-# Add your outputs here
+# 300-compute - Outputs
+
+# Web Server Outputs
+output "web_server_ids" {
+  description = "List of web server EC2 instance IDs"
+  value       = aws_instance.web[*].id
+}
+
+output "web_server_public_ips" {
+  description = "List of web server public IP addresses"
+  value       = aws_instance.web[*].public_ip
+}
+
+output "web_server_private_ips" {
+  description = "List of web server private IP addresses"
+  value       = aws_instance.web[*].private_ip
+}
